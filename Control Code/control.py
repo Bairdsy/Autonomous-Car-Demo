5bffb6860770c3ccead4065e53f1c33b6348f81d
Bairdsy <ryan@madcalm.com>
2020-01-03 09:39:51 +1100

    control code

---
+++ Control Code/control.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import time
+from neopixel import *
+
+# LED strip configuration:
+LED_COUNT      = 3      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 204     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    try:
+
+        while True:
+            light = input("select LED (0-2):")
+            red   = input("enter RED amount (0-255):")
+            blue  = input("enter BLUE amount (0-255):")
+            green = input("enter GREEN amount (0-255):")
+            strip.setPixelColor(light, Color(green,red,blue))
+            strip.show()
+
+    except KeyboardInterrupt:
+        colorWipe(strip, Color(0,0,0), 10)
